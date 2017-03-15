cdb2ed95f2c112c8e0d41827fd1ec518524f2d33
Stephen Brayson <stephen.r.brayson@CPX-WXPGTLHFGM4.dir.svc.accenture.com>
2017-03-15 13:23:13 +0000

    deleted test class

--- src/com/hmsdevelopment/teachingassistant/LoginScreen.java
+++ src/com/hmsdevelopment/teachingassistant/LoginScreen.java
@@ -13,7 +13,7 @@ public class LoginScreen extends javax.swing.JFrame {
         }
         
     }
-//hello there
+
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
