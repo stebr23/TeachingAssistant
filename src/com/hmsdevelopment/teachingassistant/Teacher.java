4f873c1c2d34f49b301f4d96dd5e2815c2a3b875
Stephen Brayson <stephen.r.brayson@192.168.56.1>
2017-04-25 00:44:38 +0100

    Changed "Email Support" to just "Email" to match the value in the Student Message screen

--- src/com/hmsdevelopment/teachingassistant/Teacher.java
+++ src/com/hmsdevelopment/teachingassistant/Teacher.java
@@ -29,7 +29,7 @@ public class Teacher extends javax.swing.JFrame {
             "Low",
             "Medium",
             "High",
-            "Email Support"
+            "Email"
         }));
         getMessageForCourseFromDB();
     }
